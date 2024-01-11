ca01fcfc9578ddfdaa5beea7dc61ee248c9fe775
DavideTarantino <davidetarantino2005@gmail.com>
2024-01-11 15:42:27 +0100

    jsnack1

---
+++ jsnack1/script.js
@@ -0,0 +1,25 @@
+let
+numero1 = prompt("inserisci un numero")
+numero2 = prompt("inserisci il secondo")
+numero3 = prompt("inserisci il terzo")
+numero4 = prompt("inserisci il quarto")
+numero5 = prompt("inserisci il quinto")
+numero6 = prompt("inserisci il sesto")
+numero7 = prompt("inserisci il settimo")
+numero8 = prompt("inserisci l'ottavo")
+numero9 = prompt("inserisci il nono")
+numero10 = prompt("inserisci il decimo")
+
+document.getElementById("responso1").innerHTML = numero1
+document.getElementById("responso2").innerHTML = numero2
+document.getElementById("responso3").innerHTML = numero3
+document.getElementById("responso4").innerHTML = numero4
+document.getElementById("responso5").innerHTML = numero5
+document.getElementById("responso6").innerHTML = numero6
+document.getElementById("responso7").innerHTML = numero7
+document.getElementById("responso8").innerHTML = numero8
+document.getElementById("responso9").innerHTML = numero9
+document.getElementById("responso10").innerHTML = numero10
+
+
+
